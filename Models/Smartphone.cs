e4b8144a7fcfdf33691ee317ea9250d45c7a824a
unknown <marcelo.boas@fatec.sp.gov.br>
2022-10-26 16:49:00 -0300

    metodos privados

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,21 +3,21 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        private string Modelo;
-        private string IMEI;
-        private int Memoria;
+        private string _Modelo;
+        private string _IMEI;
+        private int _Memoria;
 
     public void Ficha(){
-            Console.WriteLine($"Numero: {Numero}\nModelo: {Modelo}\nImei: {IMEI}\nMemória: {Memoria}");
+            Console.WriteLine($"Numero: {Numero}\nModelo: {_Modelo}\nImei: {_IMEI}\nMemória: {_Memoria}");
         }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
         public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            Modelo = modelo;
-            IMEI = imei;
-            Memoria = memoria;
+            _Modelo = modelo;
+            _IMEI = imei;
+            _Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
